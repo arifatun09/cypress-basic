ea6a35ad9f4b55570ab783deda873bb5bd3b0b50
arifatun09 <arifatunnisa.090501@gmail.com>
2023-12-12 01:03:25 +0700

    edit

--- cypress/e2e/user-can-edit-existing-data.cy.js
+++ cypress/e2e/user-can-edit-existing-data.cy.js
@@ -19,20 +19,20 @@ describe('User Can Edit Existing Data', () => {
     cy.visit('http://127.0.0.1:8000/user-management/user')
   });
   
-  //positive test case
-  // it('User can edit existing data', () => {
-  //   cy.get('.table td').contains('user').parent().find('a').contains('Edit').click();
-  //   //make sure swett alert visible
-  //   cy.get('#name').clear('user ');
-  //   cy.get('#name').type('user edited');
-  //   cy.get('.btn-primary').contains('Submit').click();
-  //   cy.get('.table td').contains('user').should('have.text', 'user edited');
-  //   cy.get(".alert")
-  //     .should("be.visible")
-  //     .and("have.class", "alert-success")
-  //     .and("contain", "User Berhasil Diupdate")
-  //   ;
-  // })
+  // positive test case
+  it('User can edit existing data', () => {
+    cy.get('.table td').contains('user').parent().find('a').contains('Edit').click();
+    //make sure swett alert visible
+    cy.get('#name').clear('user ');
+    cy.get('#name').type('user edited');
+    cy.get('.btn-primary').contains('Submit').click();
+    cy.get('.table td').contains('user').should('have.text', 'user edited');
+    cy.get(".alert")
+      .should("be.visible")
+      .and("have.class", "alert-success")
+      .and("contain", "User Berhasil Diupdate")
+    ;
+  })
 //positive test case 
 //UTS
   it.only('User can edit existing data user baru', () => {
